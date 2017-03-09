da481d44bae033bd17584244312bd4ad2191ed36
gustavogripe <gustavo@sant.at>
2017-03-09 00:25:19 -0300

    Update

---
+++ config/connect.php
@@ -0,0 +1,33 @@
+<?php
+
+// Make a MySQL Connection
+mysql_connect("mysql.santamotion.com", "santapv", "santa18") or die(mysql_error());
+mysql_select_db("santapv") or die(mysql_error());
+
+session_start("santapv");
+$user_id =  $_SESSION['userid'];
+
+$userid =  $_SESSION['userid'];
+
+
+
+$path = "http://www.santatransmedia.com/santapv/";
+
+
+    
+if($user_id==""){
+        $url="http://www.santatransmedia.com/".$_SERVER['REQUEST_URI'];
+        header( 'Location:'.$path.'login.php?url='.$url );
+        
+    }
+    
+
+
+$today = date("d/m/Y");
+
+
+
+    
+
+
+?>
